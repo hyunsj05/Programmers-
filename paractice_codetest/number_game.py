a58b786c7f15fac7160860cd18dc5acc2102dd9e
hyunsj05 <43935905+hyunsj05@users.noreply.github.com>
2021-03-03 21:08:45 +0900

    Create number_game.py

---
+++ paractice_codetest/number_game.py
@@ -0,0 +1,26 @@
+def solution(A, B):
+    A.sort(reverse=True)
+    B.sort(reverse=True)
+    answer=0
+    for i in A:
+        flag=0
+        for po,j in enumerate(B):
+            if j<i and flag==0:
+                del B[len(B)-1]
+                break
+            elif j<i:
+                del B[po-1]
+                answer+=1
+                break
+            elif len(B)-1==po:
+                if j==i:
+                    del B[po]
+                else:
+                    del B[po]
+                    answer+=1
+            flag=1
+                
+                
+    return answer
+  
+  # need to make more effiency
